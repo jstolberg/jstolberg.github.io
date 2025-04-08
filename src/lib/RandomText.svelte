e33318d741291525fe93b13f7b7459e562d37e2a
jstolberg <jsl731@protonmail.com>
2025-04-08 22:24:15 +0200

    remove superflous component.

---
+++ src/lib/RandomText.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let {width, height, top, left, innerHtml} = $props();
+    let style = 
+        'width: {width};' +
+        'height: {height];' + 
+        'top: {top};' +
+        'left: {left};';
+
+    let x = Math.random()*width;
+    let y = Math.random()*height;
+</script>
+
+<div class="randomText" style={style}>{@html innerHtml}</div>
